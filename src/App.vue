a0939d96361024614776585edb4621eb47f2332d
张杨 <443635978@qq.com>
2021-06-22 17:59:10 +0800

    animation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
         <div id="container"></div>
+        <div id="vertexShader"></div>
+        <div id="fragmentShader"></div>
     </div>
 </template>
 
 <script>
     import * as THREE from 'three';
     import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-    let moon, earth;
+    const plants = {};
+    const track = {};
 
     export default {
         name: 'App',
@@ -23,56 +26,117 @@
             }
         },
         mounted() {
-            // this.init();
             this.createScence().then(() => {
-                this.createEarth();
-                this.createMoon();
-                this.createLineCircle();
+                this.createPlant('sun', {
+                    map: 'sun.jpg',
+                    radius: 10, // 436
+                    light: true,
+                }).then(sun => {
+                    this.createPlant('mercury', {
+                        map: 'venus.jpg',
+                        radius: 1.2,
+                        track: this.createLineCircle(sun.position.x, sun.position.y, 30, 30, 2000)
+                    });
+                    this.createPlant('venus', {
+                        map: 'venus.jpg',
+                        radius: 1.5,
+                        track: this.createLineCircle(sun.position.x, sun.position.y, 50, 50, 4000)
+                    });
+                    this.createPlant('earth', {
+                        map: 'earth.png',
+                        radius: 4,
+                        track: this.createLineCircle(sun.position.x, sun.position.y, 80, 80, 20000)
+                    }).then(earth => {
+                        this.createPlant('moon', {
+                            map: 'moon.jpg',
+                            radius: 1,
+                            track: this.createLineCircle(earth.position.x, earth.position.y, 10, 10, 1000)
+                        });
+                    });
+                    this.createPlant('mars', {
+                        map: 'mars.jpg',
+                        radius: 4.5,
+                        track: this.createLineCircle(sun.position.x, sun.position.y, 110, 110, 25000)
+                    });
+                    this.createPlant('jupiter', {
+                        map: 'jupiter.jpg',
+                        radius: 2,
+                        track: this.createLineCircle(sun.position.x, sun.position.y, 130, 130, 28000)
+                    });
+                    this.createPlant('saturn', {
+                        map: 'venus.jpg',
+                        radius: 4,
+                        track: this.createLineCircle(sun.position.x, sun.position.y, 150, 150, 30000)
+                    });
+                    this.createPlant('uranus', {
+                        map: 'venus.jpg',
+                        radius: 3,
+                        track: this.createLineCircle(sun.position.x, sun.position.y, 180, 180, 26000)
+                    });
+                    this.createPlant('neptune', {
+                        map: 'venus.jpg',
+                        radius: 2,
+                        track: this.createLineCircle(sun.position.x, sun.position.y, 200, 200, 22000)
+                    });
+                    this.createPlant('pluto', {
+                        map: 'venus.jpg',
+                        radius: 2,
+                        track: this.createLineCircle(sun.position.x, sun.position.y, 230, 230, 17000)
+                    });
+                });
+
             });
         },
         methods: {
-            animation(time) {
-                if(earth) {
-                    earth.rotation.y += 0.01;
-                }
-                if(moon) {
-                    const points = this.createLineCircle(0, 0, 330);
-                    moon.position.x = points[((time)%points.length).toFixed(0)].x;
-                    moon.position.y = points[((time)%points.length).toFixed(0)].y;
-                }
-                this.controls.update();
-                this.renderer.render( this.scene, this.camera );
+            createAimation(name, time, params) {
+                return new Promise(resolve => {
+                    if(plants[name]) {
+                        plants[name].rotation.y += 0.01;
+                        if(track[name]) {
+                            const points = track[name];
+                            const deviationX = plants[params.follow] ? plants[params.follow].position.x : 0;
+                            const deviationY = plants[params.follow] ? plants[params.follow].position.y : 0;
+                            if(plants[name] && plants[name].position) {
+                                plants[name].position.x =
+                                    points[((time) % points.length).toFixed(0)].x + deviationX;
+                                plants[name].position.y =
+                                    points[((time) % points.length).toFixed(0)].y + deviationY;
+                            }
+                        }
+                        resolve(name);
+                    }
+                });
             },
             createScence() {
                 return new Promise(resolve => {
-                    this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 5000 );
+                    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight,100, 5000 );
                     this.camera.position.x = 0;
                     this.camera.position.y = 5;
-                    this.camera.position.z = 800;
+                    this.camera.position.z = 1000;
 
                     this.scene = new THREE.Scene();
 
                     // 网格
                     // const size = 500;
-                    // const divisions = 100;
+                    // const divisions = 10;
                     // const gridHelper = new THREE.GridHelper( size, divisions, 'yellow', 'green'  );
                     // this.scene.add( gridHelper );
 
                     // 点光源
                     // 背景光
-                    const backLight = new THREE.PointLight( 0xffffff, .2, 20, 2 );
-                    backLight.position.set( -3, -3, -4 );
-                    backLight.castShadow = false;
-                    this.scene.add( backLight );
+                    // const backLight = new THREE.PointLight( 0xffffff, .2, 20, 2 );
+                    // backLight.position.set( -3, -3, -4 );
+                    // backLight.castShadow = false;
+                    // this.scene.add( backLight );
 
                     // 主光源
-                    const light = new THREE.PointLight( 0xffffff, 1, 100, 2 );
-                    light.position.set( 2, 3, 4 );
+                    const light = new THREE.PointLight( 0xffffff, 2, 500, 1 );
+                    light.position.set( 0, 0, 0 );
                     light.castShadow = false;
                     this.scene.add( light );
 
                     // 环境光
-                    const sceneLight = new THREE.AmbientLight( 0xffffff, 2 ); // soft white light
+                    const sceneLight = new THREE.AmbientLight( 0xffffff, .1 ); // soft white light
                     this.scene.add( sceneLight );
 
                     // 平行光
@@ -80,6 +144,20 @@
                     directionalLight.position.set( -5, 2, 0 );
                     directionalLight.position.normalize();
                     this.scene.add( directionalLight );
+
+                    // 背景
+                    const vertices = [];
+                    for ( let i = 0; i < 5000; i ++ ) {
+                        const x = THREE.MathUtils.randFloatSpread( 2000 );
+                        const y = THREE.MathUtils.randFloatSpread( 2000 );
+                        const z = THREE.MathUtils.randFloatSpread( 2000 );
+                        vertices.push( x, y, z );
+                    }
+                    const geometry = new THREE.BufferGeometry();
+                    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+                    const material = new THREE.PointsMaterial( { color: 0x888888 } );
+                    const points = new THREE.Points( geometry, material );
+                    this.scene.add( points );
 
                     // control
                     this.renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -96,67 +174,80 @@
                     resolve();
                 });
             },
-            createEarth() {
-                const geometry = new THREE.SphereGeometry( 11, 128, 128 );
-                // 材质
-                const loader = new THREE.TextureLoader();
-                loader.setPath( './assets/' );
-
-                loader.load( 'earth.jpg', ( texture ) => {
-                    const material = new THREE.MeshStandardMaterial( {
-                        color: 0xffffff,
-                        side: THREE.FrontSide,
-                        map: texture,
-                    });
-
-                    earth = new THREE.Mesh( geometry, material );
-                    earth.position.x = 0;
-                    earth.position.y = 0;
-                    earth.matrixAutoUpdate = true;
-                    earth.receiveShadow = true;
-                    this.scene.add(earth);
-                } );
-            },
-            createMoon() {
-                const geometry = new THREE.SphereGeometry( 3, 128, 128 );
-                // 材质
-                const loader = new THREE.TextureLoader();
-                loader.setPath( './assets/' );
-
-                loader.load( 'moon.jpg', ( texture ) => {
-                    const material = new THREE.MeshStandardMaterial( {
-                        color: 0xffffff,
-                        side: THREE.FrontSide,
-                        map: texture,
-                    });
-
-                    moon = new THREE.Mesh( geometry, material );
-                    moon.position.x = -330;
-                    moon.position.y = 0;
-                    moon.matrixAutoUpdate = true;
-                    moon.receiveShadow = true;
-                    this.scene.add(moon);
-                } );
-            },
-            createLineCircle(x, y, radius) {
+            createLineCircle(x = 0, y = 0, radiusX, radiusY, speed = 1, clockwise = true) {
                 const curve = new THREE.EllipseCurve(
                     x,  y,            // ax, aY
-                    radius, radius,           // xRadius, yRadius
+                    radiusX, radiusY,           // xRadius, yRadius
                     0,  2 * Math.PI,  // aStartAngle, aEndAngle
-                    true,            // aClockwise
+                    clockwise,            // aClockwise
                     90,                 // aRotation
                 );
-                const points = curve.getPoints( 20000 );
+                const points = curve.getPoints( (speed - 1) || 0 );
 
-                // const geometry = new THREE.BufferGeometry().setFromPoints( points );
-                // const material = new THREE.LineBasicMaterial( {
-                //     color : 0xdedede,
-                //     linewidth: 1,
-                // } );
-                // const ellipse = new THREE.Line( geometry, material );
-                // this.scene.add( ellipse );
+                const geometry = new THREE.BufferGeometry().setFromPoints( points );
+                const material = new THREE.LineBasicMaterial( {
+                    color : 'rgb(20, 20, 20)',
+                    linewidth: 1,
+                } );
+                const ellipse = new THREE.Line( geometry, material );
+                this.scene.add( ellipse );
 
                 return points;
+            },
+            createPlant(name, params) {
+                return new Promise(resolve => {
+                    const geometry = new THREE.SphereGeometry( params.radius, 128, 128 );
+                    // 材质
+                    const loader = new THREE.TextureLoader();
+                    loader.setPath( './assets/' );
+
+                    loader.load( params.map, ( texture ) => {
+                        let material;
+                        if(params.light) {
+                            material = new THREE.MeshStandardMaterial({
+                                side: THREE.FrontSide,
+                                color: 0xffffff,
+                                map: texture,
+                                emissive: 0xffffff,
+                                emissiveMap: texture,
+                            });
+                        } else {
+                            material = new THREE.MeshStandardMaterial({
+                                side: THREE.FrontSide,
+                                color: 0xffffff,
+                                map: texture,
+                            });
+                        }
+
+                        plants[name] = new THREE.Mesh( geometry, material );
+
+                        plants[name].matrixAutoUpdate = true;
+                        plants[name].receiveShadow = true;
+                        this.scene.add(plants[name]);
+                        if(params.track) {
+                            track[name] = params.track;
+                        }
+                        resolve(plants[name]);
+                    } );
+                });
+
+            },
+            animation(time) {
+                this.createAimation('sun', time).then((sun) => {
+                    this.createAimation('mercury', time, { follow: sun });
+                    this.createAimation('venus', time, { follow: sun });
+                    this.createAimation('earth', time, { follow: sun }).then((earth) => {
+                        this.createAimation('moon', time, { follow: earth });
+                    });
+                    this.createAimation('mars', time, { follow: sun });
+                    this.createAimation('jupiter', time, { follow: sun });
+                    this.createAimation('saturn', time, { follow: sun });
+                    this.createAimation('neptune', time, { follow: sun });
+                    this.createAimation('pluto', time, { follow: sun });
+                });
+
+                this.controls.update();
+                this.renderer.render( this.scene, this.camera );
             },
         }
     }
